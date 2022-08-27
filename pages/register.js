f8ff7536f1265eb4ef3390658db32690e230a9a2
Restyled.io <commits@restyled.io>
2022-08-27 11:34:23 +0000

    Restyled by prettier

--- pages/register.js
+++ pages/register.js
@@ -1,13 +1,13 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import {useRouter} from 'next/router';
-import {useState} from 'react';
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 // import { DataContext } from "../store/GlobalState";
 // import { postData } from "../utils/fetchData";
 // import valid from '../utils/valid';
 
 const Register = () => {
-  const initialState = { name: '', email: '', password: '', cf_password: '' };
+  const initialState = { name: "", email: "", password: "", cf_password: "" };
   const [userData, setUserData] = useState(initialState);
   const { name, email, password, cf_password } = userData;
 
@@ -47,70 +47,70 @@ const Register = () => {
       </Head>
 
       <form
-        className='mx-auto my-4'
-        style={{ maxWidth: '500px' }}
+        className="mx-auto my-4"
+        style={{ maxWidth: "500px" }}
         onSubmit={handleSubmit}
       >
-        <div className='form-group'>
-          <label htmlFor='name'>Name</label>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
           <input
-            type='text'
-            className='form-control'
-            id='name'
-            name='name'
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
             value={name}
             onChange={handleChangeInput}
           />
         </div>
 
-        <div className='form-group'>
-          <label htmlFor='exampleInputEmail1'>Email address</label>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
           <input
-            type='email'
-            className='form-control'
-            id='exampleInputEmail1'
-            aria-describedby='emailHelp'
-            name='email'
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            name="email"
             value={email}
             onChange={handleChangeInput}
           />
-          <small id='emailHelp' className='form-text text-muted'>
+          <small id="emailHelp" className="form-text text-muted">
             We will never share your email with anyone else.
           </small>
         </div>
 
-        <div className='form-group'>
-          <label htmlFor='exampleInputPassword1'>Password</label>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
           <input
-            type='password'
-            className='form-control'
-            id='exampleInputPassword1'
-            name='password'
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            name="password"
             value={password}
             onChange={handleChangeInput}
           />
         </div>
 
-        <div className='form-group'>
-          <label htmlFor='exampleInputPassword2'>Confirm Password</label>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword2">Confirm Password</label>
           <input
-            type='password'
-            className='form-control'
-            id='exampleInputPassword2'
-            name='cf_password'
+            type="password"
+            className="form-control"
+            id="exampleInputPassword2"
+            name="cf_password"
             value={cf_password}
             onChange={handleChangeInput}
           />
         </div>
 
-        <button type='submit' className='btn btn-dark w-100'>
+        <button type="submit" className="btn btn-dark w-100">
           Register
         </button>
 
-        <p className='my-2'>
-          Already have an account?{' '}
-          <Link href='/signin'>
-            <a style={{ color: 'crimson' }}>Login Now</a>
+        <p className="my-2">
+          Already have an account?{" "}
+          <Link href="/signin">
+            <a style={{ color: "crimson" }}>Login Now</a>
           </Link>
         </p>
       </form>
