d3114589d71feb8b568e39aeca0eb2c9b135bca2
Anwar Hossain <mahedianwar@gmail.com>
2022-08-27 17:48:23 +0600

    design notify

---
+++ components/Notify.jsx
@@ -0,0 +1,39 @@
+//import { useContext } from "react";
+//import { DataContext } from "../store/GlobalState";
+import Loading from "./Loading";
+import Toast from "./Toast";
+
+const Notify = () => {
+  //const { state, dispatch } = useContext(DataContext);
+  //const { notify } = state;
+  const notify = {
+    title: "",
+    msg: "",
+    loading: false,
+    error: false,
+    success: false,
+  }
+
+  return (
+    <>
+      {notify.loading && <Loading />}
+      {notify.error && (
+        <Toast
+          msg={{ msg: notify.error, title: "Error" }}
+          handleShow={() => dispatch({ type: "NOTIFY", payload: {} })}
+          bgColor="bg-danger"
+        />
+      )}
+
+      {notify.success && (
+        <Toast
+          msg={{ msg: notify.success, title: "Success" }}
+          handleShow={() => dispatch({ type: "NOTIFY", payload: {} })}
+          bgColor="bg-success"
+        />
+      )}
+    </>
+  );
+};
+
+export default Notify;
